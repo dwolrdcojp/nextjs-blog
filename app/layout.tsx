e61815eb42bfc6ee9feb5515bfc124b6dc1cb386
Max Schrementi <6629474+dwolrdcojp@users.noreply.github.com>
2022-11-12 08:49:47 -0500

    fix hot reload bug with dom mismatch from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,10 @@
-// `pages/_app.js`
-import '../styles/global.css';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../components/layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
-
-const name = 'Max Schrementi';
-export default function RootLayout({ children}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <div className={styles.container}>
-      <header className={styles.header}>
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-      </header>
-      <main>{children}</main>
-    </div>
+    {
+    }
+    <head />
+    <body>{children}</body>
     </html>
   );
 }
