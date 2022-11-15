3032364ea0d8fa3b67e85438953a98e1663a045a
Max Schrementi <6629474+dwolrdcojp@users.noreply.github.com>
2022-11-15 16:58:40 -0500

    change fetch url

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 async function getRecentPosts(page) {
   const pageNum = page ? page : 1;
-  const recentPosts = await fetch(`/api/posts?page=${pageNum}`);
+  const recentPosts = await fetch(process.env.BASE_URL + `/api/posts?page=${pageNum}`);
 
   if (!recentPosts.ok) {
     throw new Error('Failed to fetch data');
