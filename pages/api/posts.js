2dd0c0c940f29df969f4d1c70b3a5c020b7734f5
Max Schrementi <6629474+dwolrdcojp@users.noreply.github.com>
2022-11-15 20:53:46 -0500

    change fetch url1

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -2,7 +2,7 @@ import { getPosts } from '../../firebase';
 
 export default async function handler(req, res) {
   const { page } = req.query;
-  const pageNum = page * 1;
+  const pageNum = page ? page * 1 : 1;
   const result = await getPosts(pageNum);
   res.status(200).json({ result })
 }
