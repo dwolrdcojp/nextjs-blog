5ebb81d8ccefee6f098fb9e0817ffd36f6db36d6
Max Schrementi <6629474+dwolrdcojp@users.noreply.github.com>
2022-11-16 16:52:10 -0500

    add search posts

--- app/HomePage.js
+++ app/HomePage.js
@@ -9,13 +9,30 @@ import { useState } from 'react';
 
 export default function HomePage({ recentPosts }) {
   const [pageIndex, setPageIndex] = useState(1);
+  const [posts, setPosts] = useState(recentPosts);
+  const [search, setSearch] = useState('');
   const postsCount = recentPosts.length;
   const pageCount = Math.floor((postsCount / 5)) + 1;
 
   const prevIsEnabled = pageIndex > 1 ? false : true;
   const nextIsEnabled = pageIndex < pageCount ? false : true;
 
-  const posts = recentPosts.slice((pageIndex-1) * 5, pageIndex * 5);
+  const fivePosts = posts.slice((pageIndex-1) * 5, pageIndex * 5);
+  console.log(posts);
+
+
+  function handleChange(event) {
+    const searchedPosts = posts.filter((post) => {
+      return post.title.toLowerCase().includes(event.target.value.toLowerCase())}
+    )
+    if (searchedPosts !== null) {
+      setPosts(searchedPosts)
+    }
+    setSearch(event.target.value);
+    if (event.target.value === '') {
+      setPosts(recentPosts)
+    }
+  }
 
   return (
     <div>
@@ -24,17 +41,19 @@ export default function HomePage({ recentPosts }) {
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {posts.map(({ id, title, date }) => (
-            <li className={utilStyles.listItem} key={title}>
-              <Link href={`/${id}`} legacyBehavior>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <p>{date}</p>
-              </small>
-            </li>
-          ))}
-        </ul>
+        <div className={utilStyles.posts}>
+          <ul className={utilStyles.list}>
+            {fivePosts && fivePosts.map(({ id, title, date }) => (
+              <li className={utilStyles.listItem} key={title}>
+                <Link href={`/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <p>{date}</p>
+                </small>
+              </li>
+            ))}
+          </ul>
+        </div>
         <br />
         Page {pageIndex} 
         <br />
@@ -51,7 +70,12 @@ export default function HomePage({ recentPosts }) {
             onClick={() => setPageIndex(pageIndex + 1)}> 
           Next →
         </button>
-        <br />
+        {"  "}  
+        <input 
+            className={utilStyles.search} 
+            placeholder='Search posts...' 
+            value={search} 
+            onChange={handleChange}></input>
         <br />
         <Link href={`/newpost/`} >New Post</Link>
       </section>
